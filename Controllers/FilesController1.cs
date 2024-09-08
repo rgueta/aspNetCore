97dce35d3f24431123985d51fbddb11ad39730b5
Ricardo Gueta <ricardogueta@hotmail.com>
2024-09-07 21:38:43 -0700

    revert last commit Set 7 2024 at 21:01

---
+++ Controllers/FilesController1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace aspNetCore.Controllers
+{
+    public class FilesController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
